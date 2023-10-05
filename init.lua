236317b71a616450ea86c4f71b018f1395c9df02
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-10-04 23:40:03 -0700

    Add new parsers and lsp

--- init.lua
+++ init.lua
@@ -320,7 +320,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'zig', 'astro' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'zig', 'html', 'css', 'astro' },
   
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
@@ -460,8 +460,9 @@ local servers = {
   rust_analyzer = {},
   tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs'} },
-  zls = {},
+  cssls = { filetypes = { 'css', 'scss', 'less' } },
   astro = {},
+  zls = {},
 
   lua_ls = {
     Lua = {
