8a2873f565f13046f3a9f0840acc9ea30d08fd19
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2024-05-10 15:02:03 -0700

    Add markdown treesitter

--- init.lua
+++ init.lua
@@ -494,7 +494,7 @@ vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-      'bash', 'zig', 'html', 'css', 'astro', 'json', 'xml', 'scala', 'cmake', 'c_sharp', 'glsl', 'wgsl' },
+      'bash', 'zig', 'html', 'css', 'astro', 'json', 'xml', 'scala', 'cmake', 'c_sharp', 'glsl', 'wgsl', 'markdown' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
